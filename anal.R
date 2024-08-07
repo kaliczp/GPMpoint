13398d9687ff4af3c6a6587659907f60aacea129
Kalicz Péter <kaliczp@gmail.com>
2024-08-07 10:40:45 +0200

    Daily precipitation plot

--- anal.R
+++ anal.R
@@ -1,2 +1,4 @@
 sum(raw[,2]) # 154.517 mm
 daily.sum <- tapply(raw[,2], format.POSIXct(ido, "%Y-%m-%d"), sum)
+plot(as.Date(names(daily.sum)), as.numeric(daily.sum), type = "h",
+     xlab = "", ylab = "Precipitation [mm/day]")
